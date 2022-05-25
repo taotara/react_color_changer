a9b5b3bcd4c21a813b9cf22de7f906fcec9c1697
taotara <tatoara@yahoo.com>
2022-05-25 09:59:28 +0100

    First push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Square from "./Square";
+import Input from "./Input";
+
+function App() {
+  return (
+    <div className="App">
+      <Square />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
