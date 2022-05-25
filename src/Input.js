8a5bf635d0c36439d2602e017d0dc9c808f7ce2f
taotara <tatoara@yahoo.com>
2022-05-25 10:19:02 +0100

    added a form input

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({ colorValue, setColorValue }) => {
   return (
-    <div>
-        <h2>Input</h2>
-    </div>
+    <form onSubmit={(e) => e.preventDefault()}>
+        <label>Add Color Name:</label>
+        <input 
+        autoFocus
+        type="text"
+        placeholder="Add color name"
+        require
+        value={colorValue}
+        onChange={(e) => setColorValue(e.target.value)} 
+    />
+    </form>
   )
 }
 
